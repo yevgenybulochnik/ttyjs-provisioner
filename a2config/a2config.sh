d4fdf59fd2c78ab047568a54149d8986b88f6ba2
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-05-01 16:40:49 +0000

    Initial apache2 config shell script, vim apache syntax

---
+++ a2config/a2config.sh
@@ -0,0 +1,14 @@
+#Install apache2
+apt install apache2
+
+#Enable mods
+a2enmod proxy
+a2enmod proxy_http
+a2enmod proxy_wstunnel
+a2enmod rewrite
+
+#Disable default apache site
+a2dissite 000-default.conf
+
+#Remove default /var/www/html
+rm -rf /var/www/html
