a8d2a7ee306ab1573371817e9ede4d5c69e8d682
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-05-03 18:51:24 -0700

    SHELL_DIR absolute path, execute a2config.sh

--- ttyjs_provisioner.sh
+++ ttyjs_provisioner.sh
@@ -1,3 +1,6 @@
+#Absolute path of executed shellscript 
+SHELL_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
+
 #Updating APT Repos
 apt update
 
@@ -5,3 +8,6 @@ apt update
 adduser --disabled-password --gecos "" yevgeny
 usermod -aG sudo yevgeny
 echo "yevgeny ALL=(ALL) NOPASSWD:ALL" >>  /etc/sudoers #passwordless sudo
+
+#Execute a2config shellscript
+${DIR}/a2config/a2config.sh
