4f52128510a63131ad2732db883a44ca85514b3c
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-05-07 10:05:27 -0700

    Apt, Npm, vim package install

--- ttyjs_provisioner.sh
+++ ttyjs_provisioner.sh
@@ -11,3 +11,21 @@ echo "yevgeny ALL=(ALL) NOPASSWD:ALL" >>  /etc/sudoers #passwordless sudo
 
 #Execute a2config shellscript
 ${SHELL_DIR}/a2config/a2config.sh
+
+#Install apt packages
+apt install -y \
+    npm \
+    htop 
+
+#Install npm packages +Node
+npm install -g n 
+n 6.10.1
+npm install -g pm2 
+
+#Setup Dotfiles
+
+su yevgeny -c "git clone https://github.com/yevgenybulochnik/dotfiles.git ~/dotfiles"
+su yevgeny -c "~/dotfiles/install.sh"
+
+su yevgeny -c "mkdir ~/.ssh"
+su yevgeny -c "curl https://github.com/yevgenybulochnik.keys > ~/.ssh/authorized_keys"
