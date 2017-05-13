24ffcca92e69688584ae7952d1e09f31ec19e884
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-05-12 22:13:54 -0700

    Created $USERNAME command heredoc

--- ttyjs_provisioner.sh
+++ ttyjs_provisioner.sh
@@ -27,12 +27,13 @@ npm install -g n
 n 6.10.1
 npm install -g pm2
 
-#Setup Dotfiles
-su $USERNAME -c "git clone https://github.com/yevgenybulochnik/dotfiles.git ~/dotfiles"
-su $USERNAME -c "~/dotfiles/install.sh"
-
-su $USERNAME -c "mkdir ~/.ssh"
-su $USERNAME -c "curl https://github.com/yevgenybulochnik.keys > ~/.ssh/authorized_keys"
-su $USERNAME -c "sudo npm install -g tty.js"
-su $USERNAME -c "sudo pm2 start tty.js -- -p 8181"
-su $USERNAME -c "sudo htpasswd -cb /etc/apache2/.htpasswd $USERNAME $PASSWD"
+#Setup Dotfiles, ttyjs, pm2, htpasswd
+su $USERNAME <<COMMANDS
+mkdir ~/.ssh
+curl https://github.com/yevgenybulochnik.keys > ~/.ssh/authorized_keys
+sudo npm install -g tty.js
+sudo pm2 start tty.js -- -p 8181
+sudo htpasswd -cb /etc/apache2/.htpasswd $USERNAME $PASSWD
+git clone https://github.com/yevgenybulochnik/dotfiles.git ~/dotfiles
+~/dotfiles/install.sh
+COMMANDS
